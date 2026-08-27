badb0a7940e955bf201352c0eebba7dd2e617ea4
Likita169 <157664973+Likita169@users.noreply.github.com>
2026-08-27 17:49:26 +0700

    Update APP_VERSION to 1.5.1

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
 // Sửa DUY NHẤT dòng này mỗi khi deploy bản mới (1.0.0 -> 1.0.1 -> 1.1.0 ...).
 // Cả app.js (hiện số phiên bản trong tab Thống kê) và service-worker.js
-const APP_VERSION = '1.5.0';
+const APP_VERSION = '1.5.1';
